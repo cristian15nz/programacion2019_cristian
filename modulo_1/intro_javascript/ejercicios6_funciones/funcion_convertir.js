21167a0dfda8c35e52fe6f6ee0bcc47ac9dcf823
Cristian <cristian15nz@gmail.com>
2019-08-20 12:34:22 -0400

    Ejemplos de funciones y PHP

---
+++ modulo_1/intro_javascript/ejercicios6_funciones/funcion_convertir.js
@@ -0,0 +1,25 @@
+// var kilometros = parseInt(prompt("Digite los kilometros para llevarlos a metros"));
+
+// var metros = kilometros * 1000;
+
+// alert(`${kilometros}Km serian ${metros}m`);
+
+function convertirKilometrosMetros(cantidadDeKilometros) {
+    var metros = cantidadDeKilometros * 1000;
+    alert(`${cantidadDeKilometros} km llevados metros serian ${metros}`);
+}
+
+var kilometros = 7.5;
+convertirKilometrosMetros(kilometros);
+
+var kilometros = 10;
+convertirKilometrosMetros(kilometros);
+
+var kilometros = 3;
+convertirKilometrosMetros(kilometros);
+
+var kilometros = 1;
+convertirKilometrosMetros(kilometros);
+
+var kilometros = 0;
+convertirKilometrosMetros(kilometros);
---
+++ modulo_1/intro_javascript/ejercicios6_funciones/funcion_suma.js
@@ -0,0 +1,25 @@
+// Declarar mis funciones
+// function sumar(numero1, numero2) {
+//     return numero1 + numero2;
+// }
+
+function restar(numero1, numero2) {
+    return numero1 - numero2;
+}
+
+function sumar(valor1, valor2) {
+    console.log(`El primer valor es ${valor1}`);
+    console.log(`El segundo valor es ${valor2}`);
+}
+
+// Usarlas
+// var valor1 = parseInt(prompt("Digite el primer valor"));
+// var valor2 = parseInt(prompt("Digite el segundo valor"));
+
+// console.log( sumar(valor1, valor2) );
+console.log( sumar(9) );
+
+console.log( sumar(5, 3) );
+console.log( sumar(5, 3) );
+console.log( sumar("hola", "mundo") );
+
