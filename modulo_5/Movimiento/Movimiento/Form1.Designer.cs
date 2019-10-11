914de170d05cc4fe2ed5a153a4b148da5ce46ea9
Cristian <cristian15nz@gmail.com>
2019-10-11 12:22:47 -0400

    Movimiento

---
+++ modulo_5/Movimiento/Movimiento/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Movimiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(15, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 400);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.OrangeRed;
+            this.label2.Location = new System.Drawing.Point(168, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 30);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(382, 453);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ modulo_5/Movimiento/Movimiento/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Movimiento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Obtener el valor de la tecla
+            var keyValue = e.KeyValue;
+
+            var posicionX = label2.Location.X;
+            var posicionY = label2.Location.Y;
+
+            // Izquierda
+            if (keyValue == 37)
+            {
+                label2.Location = new System.Drawing.Point(posicionX - 1, posicionY);
+                label3.Text = label2.Location.ToString();
+            }
+
+            // Arriba
+            if (keyValue == 38)
+            {
+                label2.Location = new System.Drawing.Point(posicionX, posicionY - 1);
+                label3.Text = label2.Location.ToString();
+            }
+
+            // Derecha
+            if (keyValue == 39)
+            {
+                label2.Location = new System.Drawing.Point(posicionX + 1, posicionY);
+                label3.Text = label2.Location.ToString();
+            }
+
+            // Abajo
+            if (keyValue == 40)
+            {
+                label2.Location = new System.Drawing.Point(posicionX, posicionY + 1);
+                label3.Text = label2.Location.ToString();
+            }
+
+
+            // 
+        }
+    }
+}
