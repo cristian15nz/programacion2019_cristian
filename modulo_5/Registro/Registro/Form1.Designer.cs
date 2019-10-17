ccf588e0bf586dbd65c124ea65ccb0a64a660eb5
Cristian <cristian15nz@gmail.com>
2019-10-17 12:31:18 -0400

    Crear el registro

---
+++ modulo_5/Registro/Registro/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Registro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtBio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNombre.Location = new System.Drawing.Point(15, 64);
+            this.txtNombre.MaxLength = 30;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(255, 22);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtCorreo.Location = new System.Drawing.Point(15, 120);
+            this.txtCorreo.MaxLength = 30;
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(164, 22);
+            this.txtCorreo.TabIndex = 3;
+            this.txtCorreo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(15, 234);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(255, 22);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtUsername.Location = new System.Drawing.Point(15, 179);
+            this.txtUsername.MaxLength = 12;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(255, 22);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // txtBio
+            // 
+            this.txtBio.Location = new System.Drawing.Point(15, 294);
+            this.txtBio.Multiline = true;
+            this.txtBio.Name = "txtBio";
+            this.txtBio.Size = new System.Drawing.Size(255, 78);
+            this.txtBio.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Correo de Gmail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(185, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "@gmail.com";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Contraseña";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Nombre de usuario";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 271);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Bio (opcional)";
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Location = new System.Drawing.Point(15, 387);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(255, 34);
+            this.btnRegistro.TabIndex = 12;
+            this.btnRegistro.Text = "Regístrate";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            this.btnRegistro.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(190, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 17);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "10/20/3040";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 433);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnRegistro);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBio);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtBio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ modulo_5/Registro/Registro/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Registro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void conexionBaseDatos()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=github;";
+
+            // Prepara la conexión
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+
+            // Abre la base de datos
+            databaseConnection.Open();
+        }
+
+        private void SaveUser(string nombre, string correo, string username, string password, string bio)
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=github;";
+            var query = String.Format("INSERT INTO usuarios (nombre, correo, username, password, bio) VALUES (\"{0}\", \"{1}\", \"{2}\", \"{3}\", \"{4}\") ", nombre, correo, username, password, bio);
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                MessageBox.Show("Usuario insertado satisfactoriamente");
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Mostrar cualquier error
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Capturar los datos
+            var nombre = txtNombre.Text;
+            var correo = txtCorreo.Text;
+            var username = txtUsername.Text;
+            var password = txtPassword.Text;
+            var bio = txtBio.Text;
+
+            // Validar los datos
+            if (string.IsNullOrEmpty(nombre))
+            {
+                MessageBox.Show("El nombre no puede estar vacío.");
+                txtNombre.Focus();
+                return;
+            }
+
+            // Validar el correo
+            if (string.IsNullOrEmpty(correo))
+            {
+                MessageBox.Show("El correo no puede estar vacío.");
+                txtCorreo.Focus();
+                return;
+            }
+
+            // Validar el nombre de usuario
+            if (string.IsNullOrEmpty(username))
+            {
+                MessageBox.Show("El nombre de usuario no puede estar vacío.");
+                txtUsername.Focus();
+                return;
+            }
+
+            // Validar la contraseña
+            if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("La contraseña no puede estar vacía.");
+                txtPassword.Focus();
+                return;
+            }
+
+            // Preparar el query
+            /* var query = @"INSERT INTO usuarios
+            (nombre, correo, username, password, bio)
+            VALUES
+            (\"Cristian\", \"co\", \"\", \"\", \"\")";
+            */
+
+            // Concatenarle el gmail.com
+            correo += "@gmail.com";
+
+            SaveUser(nombre, correo, username, password, bio);
+
+            // Exito
+            MessageBox.Show("Usuario registrado");
+
+            // Vaciar el formulario cuando se registre el usuario
+            txtNombre.Text = "";
+            txtCorreo.Text = "";
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtBio.Text = "";
+          
+
+        }
+    }
+}
